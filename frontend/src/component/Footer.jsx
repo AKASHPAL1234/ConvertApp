c77ebcf8e49d381ddc29060dd22f91e8a0070aa2
akash pal <palankit86762@gmail.com>
2025-04-18 21:12:16 +0530

    Done !!

---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    < >
+    <div className='item-center'>
+        <hr className=' border-black
+        ' />
+    <h1 className='text-center py-3 text-sm ' >copyright©2025-All right reserved by Akash pal </h1>
+
+    </div>
+        
+    </>
+  )
+}
+
+export default Footer
