97041b5b7f95bf8575a720ed577cf31378ed1473
akash pal <palankit86762@gmail.com>
2025-04-20 12:10:20 +0530

    update

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
       const formData=new FormData()
       formData.append("file",selectFile)
       try{
-        const response = await axios.post("https://convertapp.onrender.com", formData, {
+        const response =await axios.post("https://convertapp.onrender.com/convert", formData, {
           responseType: "blob",
         });
         
